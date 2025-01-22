1d7257fe74f5f9cf129665595268d0962f5ac711
HIPgirl95 <hannah@dawne.com>
2025-01-22 16:50:05 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'myFlix-Angular-Client';
+
+  constructor(public dialog: MatDialog) {}
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
 }
