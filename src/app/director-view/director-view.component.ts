98a7af77651a7663cb10cf9808e62fbe72299535
HIPgirl95 <hannah@dawne.com>
2025-01-31 16:35:36 -0500

    adding director and genre views

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-director-view',
+  imports: [NavbarComponent],
+  templateUrl: './director-view.component.html',
+  styleUrl: './director-view.component.scss',
+})
+export class DirectorViewComponent {}
---
+++ src/app/genre-view/genre-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-genre-view',
+  imports: [NavbarComponent],
+  templateUrl: './genre-view.component.html',
+  styleUrl: './genre-view.component.scss',
+})
+export class GenreViewComponent {}
