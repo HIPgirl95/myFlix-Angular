03fbe9acda861c8813455f4de46eaa3965dc291a
HIPgirl95 <hannah@dawne.com>
2025-02-03 15:35:45 -0500

    fix navbar - add logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatListModule, RouterLink],
+  imports: [MatToolbarModule, MatListModule, RouterLink, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  logout(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    console.log(localStorage.getItem('user'));
+  }
+}
