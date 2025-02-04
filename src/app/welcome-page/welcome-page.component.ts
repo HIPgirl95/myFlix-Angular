5a882edb3785b057b3c90daeeb2f092abf1242f8
HIPgirl95 <hannah@dawne.com>
2025-02-04 18:04:54 -0500

    start of typedoc comments

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -4,6 +4,14 @@ import { UserRegistrationFormComponent } from '../user-registration-form/user-re
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 
+/**
+ * WelcomePageComponent is responsible for displaying the welcome page and
+ * opening the login and registration dialog windows
+ *
+ * @exports
+ * @class WelcomePageComponent
+ */
+
 @Component({
   selector: 'app-welcome-page',
   imports: [MatButtonModule],
@@ -11,13 +19,34 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './welcome-page.component.scss',
 })
 export class WelcomePageComponent {
+  /**
+   * Creates an instance of WelcomePageComponent
+   * @param {MatDialog} dialog - dialog service to manage opening of dialogs
+   * @memberof WelcomePageComponent
+   */
+
   constructor(public dialog: MatDialog) {}
+
+  /**
+   * Initializes the component
+   * @memberof WelcomePageComponent
+   */
   ngOnInit(): void {}
 
+  /**
+   * Open the user Registration Dialog when called. width is set to 280px
+   *
+   * @memberof WelcomePageComponent
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
   }
 
+  /**
+   * Open the user login Dialog when called. width is set to 280px
+   *
+   * @memberof WelcomePageComponent
+   */
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, { width: '280px' });
   }
